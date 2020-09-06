7d06bc5f9376f65609fac53a2345d3058330f29d
Gabor Ropos <ropos.gabor@gmail.com>
2020-09-06 09:52:15 +0200

    modify stylesheets to SASS

---
+++ src/componenet/header/Header.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import './Header.scss';
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  componentDidMount() {
+
+  }
+
+  componentWillUnmount() {
+
+  }
+
+  render() {
+    return (
+      <header className="navBar">
+        <h1>HEADER</h1>
+      </header>
+    );
+  }
+}
+
+Header.propTypes = {};
+
+export default Header;
